154c319a8f5400be39f81624d5e594da6d12457a
Asakrg <aliyusahmad01@gmail.com>
2024-03-14 02:28:19 -0700

    Initial commit

---
+++ backend/src/main/java/com/innerbyte/hotelbookingapp/HotelBookingAppApplication.java
@@ -0,0 +1,13 @@
+package com.innerbyte.hotelbookingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelBookingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelBookingAppApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/innerbyte/hotelbookingapp/HotelBookingAppApplicationTests.java
@@ -0,0 +1,14 @@
+package com.innerbyte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+
